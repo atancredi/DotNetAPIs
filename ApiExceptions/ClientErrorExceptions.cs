5a33badf66001378c339906e001d85ba7f739edf
Alessandro Tancredi <83644336+atancredi@users.noreply.github.com>
2021-10-29 16:46:11 +0200

    Aggiungere i file di progetto.

---
+++ ApiExceptions/ClientErrorExceptions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ApiExceptions
+{
+    public class BadRequest : Exception
+    {
+        public BadRequest()
+            : base("The server cannot or will not process the request due to an apparent client error.")
+        {
+
+        }
+    }
+
+    public class Unauthorized : Exception
+    {
+        public Unauthorized()
+            : base("Authentication failed")
+        {
+
+        }
+    }
+}
---
+++ ApiExceptions/ServerErrorExceptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ApiExceptions
+{
+    public class InternalServerError : Exception
+    {
+        public InternalServerError()
+            : base("An internal error occurred while processing the request.")
+        { }
+    }
+}
